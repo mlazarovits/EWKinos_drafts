f18fe197b27f82bbe19cd2c87240c5cc7a75100b
Margaret Lazarovits <margaretlazarovits@Margarets-Air-2.attlocal.net>
2019-05-27 15:14:17 -0500

    object to pointer in .c

--- src/eff_vs_met2.C
+++ src/eff_vs_met2.C
@@ -1,11 +1,11 @@
 #include "include/efficiency.h"
 
 void eff_vs_met(TString dataset){
-	efficiency eff;
+	efficiency* eff = efficiency();
 
-	eff.Initialize(dataset);
-	eff.make_metbins();
-	eff.counter();
-	eff.make_plot();
+	eff->Initialize(dataset);
+	eff->make_metbins();
+	eff->counter();
+	eff->make_plot();
 
 }
