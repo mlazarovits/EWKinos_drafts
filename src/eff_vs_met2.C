ae127dae06ef9c1cad4d288578e9b647789bd50a
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-05-28 16:16:46 -0500

    filename

--- src/eff_vs_met2.C
+++ src/eff_vs_met2.C
@@ -1,6 +1,6 @@
 #include "/home/t3-ku/mlazarov/CMSSW_10_1_4_patch1/src/EWKinos_drafts/include/efficiency.h"
 
-void eff_vs_met(TString dataset){
+void eff_vs_met2(TString dataset){
 	efficiency eff;
 
 	eff.Initialize(dataset);
