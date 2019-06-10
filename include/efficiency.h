cfa4f94dc025286646a998e57dae7a2872690fd7
Margaret Lazarovits <mrlazarovits@gmail.com>
2019-06-10 15:30:44 -0500

    debug make_plot segfault

--- include/efficiency.h
+++ include/efficiency.h
@@ -284,7 +284,7 @@ inline void efficiency::make_plot(){
 		cout << "pushback tgraph" << endl;
 		cout << filter_names[i] << endl;
 		cout << "met_plot size " << met_plot.size() << endl;
-		for(int j = 0; j < (int)tmet_plot.size();j++){
+		for(int j = 0; j < (int)met_plot.size();j++){
 			cout << "tmet_plot #" << j << " " << tmet_plot[j] << endl;
 			cout << "tmet_uncert #" << j << " " << tmet_uncert[j] << endl;
 			cout << "tNeff #" << j << " " << tNeff[j] << endl;
