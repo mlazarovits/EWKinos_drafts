552e1cda9180b8de774d00538f197e109d1a9b7b
Margaret Lazarovits <margaretlazarovits@Margarets-Air-2.attlocal.net>
2019-06-18 14:27:08 -0500

    delete file if there before updating

--- src/jwk_ewkino_make_hists.C
+++ src/jwk_ewkino_make_hists.C
@@ -120,7 +120,7 @@ void jwk_ewkino_make_hists(){
   vector<string> wjets_total = load_gFile_WJetsToLnu();
 
   if(gSystem->AccessPathName(SingLeptonTFile.c_str())){
-    gSystem->Exec("rm"+SingLeptonTFile.c_str());
+    gSystem->Exec("rm"+SingLeptonTFile);
   }
 
 //--------BackGrounds
