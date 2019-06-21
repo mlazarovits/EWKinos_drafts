b775cdc45acd3c105fdcdfa3b3b2983dbb61ca70
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-06-21 13:59:26 -0500

    added event skip

--- src/jwk_ewkino_make_hists.C
+++ src/jwk_ewkino_make_hists.C
@@ -127,7 +127,8 @@ void run_hist_maker( string treename, string g_FileName, string outfile_name, st
 void jwk_ewkino_make_hists(){
   vector<string> bkg_vec = load_gFileVec_TChiWZ_Bkg();
   vector<string> wjets_total = load_gFile_WJetsToLnu();
-  histMaker.skip = 1000;
+  histMaker hist_maker
+  hist_maker.skip = 1000;
 
 //--------BackGrounds
   run_hist_maker( BkgTree_KUAnalysis, bkg_vec,    SingLeptonTFile, fall17_TChiWZ_Bkg_SubDir );
